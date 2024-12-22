142db6c79b67db261515f0ad570963b20b94e216
quin412 <nguyenquynh41202@gmail.com>
2024-12-22 19:59:12 +0700

    show password

--- app/src/main/java/aaa/exe/nhom_12_chuong_trinh_android/common/activity/ChangePasswordActivity.java
+++ app/src/main/java/aaa/exe/nhom_12_chuong_trinh_android/common/activity/ChangePasswordActivity.java
@@ -1,6 +1,8 @@
 package aaa.exe.nhom_12_chuong_trinh_android.common.activity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,13 +17,82 @@ public class ChangePasswordActivity extends AppCompatActivity {
     EditText editEmail, editOldPassword, editNewPassword, editConfirm;
     Button btnChange;
     UserDAO userDAO;
+    boolean isPasswordVisible = false;
+    boolean isPasswordVisible2 = false;
+    boolean isPasswordVisible3 = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cm_activity_change_password);
-        ax();
+        mapping();
+        editOldPassword.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    if(motionEvent.getRawX() >= (editOldPassword.getRight() - editOldPassword.getCompoundDrawables()[2].getBounds().width())){
+                        isPasswordVisible = !isPasswordVisible;
+                        if (isPasswordVisible) {
+                            editOldPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                            editOldPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eye_open, 0); // Icon mắt mở
+                        } else {
+                            editOldPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                            editOldPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eye_close, 0); // Icon mắt đóng
+                        }
+
+                        // Đưa con trỏ văn bản về cuối
+                        editOldPassword.setSelection(editOldPassword.getText().length());
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        editNewPassword.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    if(motionEvent.getRawX() >= (editNewPassword.getRight() - editNewPassword.getCompoundDrawables()[2].getBounds().width())){
+                        isPasswordVisible2 = !isPasswordVisible2;
+                        if (isPasswordVisible2) {
+                            editNewPassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                            editNewPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eye_open, 0); // Icon mắt mở
+                        } else {
+                            editNewPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                            editNewPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eye_close, 0); // Icon mắt đóng
+                        }
+
+                        // Đưa con trỏ văn bản về cuối
+                        editNewPassword.setSelection(editNewPassword.getText().length());
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        editConfirm.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    if(motionEvent.getRawX() >= (editConfirm.getRight() - editConfirm.getCompoundDrawables()[2].getBounds().width())){
+                        isPasswordVisible3 = !isPasswordVisible3;
+                        if (isPasswordVisible3) {
+                            editConfirm.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                            editConfirm.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eye_open, 0); // Icon mắt mở
+                        } else {
+                            editConfirm.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                            editConfirm.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.eye_close, 0); // Icon mắt đóng
+                        }
+
+                        // Đưa con trỏ văn bản về cuối
+                        editConfirm.setSelection(editConfirm.getText().length());
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
-    public void ax(){
+    public void mapping(){
         userDAO = new UserDAO(this);
         editEmail = findViewById(R.id.editEmail);
         editOldPassword = findViewById(R.id.editOldPass);
